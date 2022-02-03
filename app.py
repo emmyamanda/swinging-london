351ce1fe8755ce3a48b733f09782d601b46264bc
emmyamanda <emmysignal95@hotmail.com>
2022-02-03 16:24:42 +0000

    Created registration page adding form from Materialize to create account

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for) 
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,30 @@ mongo = PyMongo(app)
 def get_tips():
     tips = mongo.db.tips.find()
     return render_template("tips.html", tips=tips)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get(
+                "username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
