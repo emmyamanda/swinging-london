85ceb1426874891cf3ee8592e83b871c1718454d
emmyamanda <emmysignal95@hotmail.com>
2022-02-03 17:46:12 +0000

    Created base for profile.html, to be continued

--- app.py
+++ app.py
@@ -73,6 +73,17 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
