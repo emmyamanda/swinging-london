56232c720b13b559af24caae890d6dfb65d5e85e
emmyamanda <emmysignal95@hotmail.com>
2022-02-22 00:35:59 +0000

    Commenting out

--- static/js/script.js
+++ static/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     });
     $('.modal').modal();
 
+    //This function has been used from Task Manager mini project with the Code Institute
     validateMaterializeSelect();
         function validateMaterializeSelect() {
             let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
